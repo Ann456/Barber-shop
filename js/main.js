f66d1fc1f03c4d6ab853c762ffdf67e89e708a28
Ann456 <popovaa769@gmail.com>
2021-11-26 22:13:17 +0200

    изменила код

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//fixed navigation
+	//fixed navigation
 	$(document).scroll (function(){
 		if ($(document).scrollTop() > 0) $('.header__wrapper').addClass('fixed-nav');
         else $('.header__wrapper').removeClass('fixed-nav');
